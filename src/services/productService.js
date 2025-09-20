2921fc497db0b7aae58a40fe5d22a63cf10de233
chatradivijayarajya-1 <chatradivijayarajya@gmail.com>
2025-09-20 09:51:37 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-52-91-52-211.compute-1.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://23.22.237.242:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://ec2-52-91-52-211.compute-1.amazonaws.com:8080/api/products/images/${imagePath}`;
+  return `http://23.22.237.242:8080/api/products/images/${imagePath}`;
 };
